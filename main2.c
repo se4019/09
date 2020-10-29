8434fc34763c8f7a4a5b926a23fdecc3323bd910
se4019 <70657061+se4019@users.noreply.github.com>
2020-10-29 15:57:34 +0900

    Initial commit

---
+++ main2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#define SIZE 5
+
+
+int main(int argc, char *argv[]) {
+	
+	//array declaration
+	int i, average;
+	int sum;
+	int grade[SIZE];
+	
+	printf("input %i scores\n", SIZE);
+	
+	sum = 0;
+	for (i=0;i<SIZE;i++)
+	{
+		scanf("%d", &grade[i]);
+		sum += grade[i];
+	}
+		
+	for (i=0;i<SIZE;i++)
+		printf("grade[%d] = %d\n", i, grade[i]);
+		
+	average = sum/SIZE;
+	printf("average : %i\n", average);
+	
+	return 0;
+}
---
+++ main3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#define SIZE 5
+
+
+int main(int argc, char *argv[]) {
+	
+	//array declaration
+	int i;
+	int grade[SIZE];
+	int score[SIZE];
+	
+	for (i=0;i<SIZE;i++)
+		grade[i] = rand()%100+1;
+		
+	
+	//move to score
+	for (i=0;i<SIZE;i++)
+		score[i] = grade[i];
+		
+	for (i=0;i<SIZE;i++)
+		printf("score[%d] %i (%i)\n", i, score[i], grade[i]);
+		
+	return 0;
+}
